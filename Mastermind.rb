75ca040aec42860ac124897de2074aba7112e3fa
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-03-28 21:22:10 -0400

    Update computerCode function array to select colors

--- Mastermind.rb
+++ Mastermind.rb
@@ -2,10 +2,27 @@ class Mastermind
 
 def computerCode
   # red = 1, green = 2, blue = 3, black = 4, white = 5, brown = 6
-code = Array.new(4) {rand(1..6)}
-print code
-end
+@code = Array.new(4) {rand(1..6)}
 
+print @code.map { |x|
+  if x == 1
+  "red"
+  elsif x == 2
+  "green"
+  elsif x == 3
+  "blue"
+  elsif x == 4
+  "black"
+  elsif x == 5
+  "white"
+  elsif x == 6
+  "brown"
+#else x
+  end
+}
+
+
+end
 end
 
 a = Mastermind.new
